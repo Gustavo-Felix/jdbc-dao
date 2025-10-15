4891677ef58c6cdcf460b56ef4f4d35a4261738d
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-10-15 16:34:52 -0300

    feat: add Seller entity class

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,13 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Department objDepartment = new Department(1, "Books");
+        System.out.println("Department - " + objDepartment);
+
+        Seller objSeller = new Seller(1, "Gustavo", "gustavo@gmail.com", LocalDate.now(), 3000.00, objDepartment);
+        System.out.println("Seller - " + objSeller);
 
     }
 
